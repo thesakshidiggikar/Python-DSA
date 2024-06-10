c10aa4b97e9702253baf717d2d2fe8615957e848
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2024-06-10 22:58:28 +0530

    Updated

---
+++ opps/methods.py
@@ -0,0 +1,60 @@
+# class Methods:
+#     @staticmethod # decorator= Function can run without "self" .
+#     def welcome():
+#         print("hello World!")
+
+# s1=Methods()
+# s1.welcome()
+
+##Abstraction  = not showing the hidden items, only showing the function.
+
+# class Cars:
+#     def __init__(self):
+#         self.clutch= False
+#         self.acc=False
+#         self.breek=False
+
+#     def start(self):
+#         self.acc=True #hidden ideam
+#         self.clutch=True #hidden item
+#         print("car Stared Brooooom!...")
+
+
+#     def stop(self):
+#         self.breek=True
+#         print("car Stopped .......!!!!")
+
+# s1=Cars()
+# s1.start()
+# s1.stop()
+
+
+## Encapsulation = Making the data to be in one capsule  or in single unit(object)
+
+# class Bankdetail:
+
+
+#     def __init__(self, balance, acc):
+#         self.balance=balance
+#         self.acc=acc
+
+#     def cr(self):
+#         self.balance += amt
+#         print("Money added")
+#     def deb(self):
+#         self.balance-= amt
+#         print("money  Went away ! ")
+
+#     def Bal(self):
+#         return self.balance
+
+# s1=Bankdetail(10000,2300)
+# amt = 1000
+# s1.cr()
+# s1.deb()
+# s1.Bal()
+
+
+# OOps Concept
+# del
+amt = 1000
