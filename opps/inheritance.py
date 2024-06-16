ebbc170b71fd89e3dad0374b48a11fa4caa9a34d
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2024-06-16 23:54:21 +0530

    Updated

---
+++ opps/inheritance.py
@@ -0,0 +1,28 @@
+#inheritance
+#single level inheritance
+# class Inherit1:                       #base class
+#     @staticmethod
+#     def start():
+#         print("Car started...")
+#     @staticmethod
+#     def stop():
+#         print("car Stopped...")
+
+# class BMW(Inherit1):                  #derived class
+#     def __init__(self,name):
+#         self.name=name
+
+# c1=BMW("Toyota")
+# print(c1.start())
+
+#Double layer inheritance
+#multi level inheritance
+
+#super keyword is used to access the method from the parent class.
+#class BMW(Inherit1):                  #derived class
+#     def __init__(self,name):
+#         self.name=name
+            #super().__init__(name)
+class BMW(Inherit1):                  #derived class
+#     def __init__(self,name):
+#         self.name=name
