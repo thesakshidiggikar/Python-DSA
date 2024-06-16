571a0ddee19da236f4ea28c49eadff13e0e96288
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2024-06-16 23:54:45 +0530

    Updated

--- opps/inheritance.py
+++ opps/inheritance.py
@@ -33,4 +33,7 @@ lass BMW(Inherit1):                  #derived class
 lass BMW(Inherit1):                  #derived class
 #     def __init__(self,name):
 #         self.name=name
+lass BMW(Inherit1):                  #derived class
+#     def __init__(self,name):
+#         self.name=name
 
