e15d60a1f4c6b49bfaa6677d96ab3d14cb8f60c1
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2024-06-21 22:40:59 +0530

    Updated

---
+++ HackerrankDSA/Arrays.py
@@ -0,0 +1,14 @@
+def reverseArray(A):
+    reverseArray = A[::-1]
+    return reverseArray
+
+
+if __name__=="__main__":
+    n = int(input().strip())
+
+# Read the array elements
+A = list(map(int, input().strip().split()))
+reversed_A = reverseArray(A)
+
+
+print(" ".join(map(str, reversed_A)))
