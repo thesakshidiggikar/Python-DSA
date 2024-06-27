d812d1be85aefc7e1beb29b48625a8cb78eebd8f
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2024-06-27 23:18:20 +0530

    Updated

---
+++ HackerrankDSA/occurs.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def matchingStrings(strings, queries):
+    frequency_dict = {}
+
+    for string in strings:
+        if string in frequency_dict:
+            frequency_dict[string] += 1
+        else:
+            frequency_dict[string] = 1
+
+    result = []
+    for query in queries:
+        result.append(frequency_dict.get(query, 0))
+
+    return result
+
+
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    fptr.write("\n".join(map(str, res)))
+    fptr.write("\n")
+
+    fptr.close()
+    fptr.close()
