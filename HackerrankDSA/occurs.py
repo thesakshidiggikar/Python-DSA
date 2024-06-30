669b70fa2244b4dc36d8b70b608d93113c3251db
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2024-06-30 23:09:31 +0530

    UPdated

--- HackerrankDSA/occurs.py
+++ HackerrankDSA/occurs.py
@@ -47,4 +47,4 @@ if __name__ == "__main__":
     fptr.write("\n")
 
     fptr.close()
-    fptr.close()fptr.close()fptr.close()fptr.close()
+    fptr.close()fptr.close()fptr.close()fptr.close()fptr.close()
