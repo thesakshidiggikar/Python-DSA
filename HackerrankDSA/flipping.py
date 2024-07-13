2267c4fc5065593ea29ec876dc3934c3da79f994
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2024-07-13 20:52:17 +0530

    Updated

--- HackerrankDSA/flipping.py
+++ HackerrankDSA/flipping.py
@@ -7,23 +7,3 @@ if __name__ == "__main__":
     for _ in range(q):
         n = int(input().strip())
         print(flippingBits(n))
-if __name__ == "__main__":
-    q = int(input().strip())
-    for _ in range(q):
-        n = int(input().strip())
-        print(flippingBits(n))
-if __name__ == "__main__":
-    q = int(input().strip())
-    for _ in range(q):
-        n = int(input().strip())
-        print(flippingBits(n))
-if __name__ == "__main__":
-    q = int(input().strip())
-    for _ in range(q):
-        n = int(input().strip())
-        print(flippingBits(n))
-if __name__ == "__main__":
-    q = int(input().strip())
-    for _ in range(q):
-        n = int(input().strip())
-        print(flippingBits(n))
