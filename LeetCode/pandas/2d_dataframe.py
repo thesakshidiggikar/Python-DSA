a95a8c8de8174c92f2a981672fef55fc57601dd2
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2024-08-27 22:10:32 +0530

    Updated

--- LeetCode/pandas/2d_dataframe.py
+++ LeetCode/pandas/2d_dataframe.py
@@ -13,3 +13,18 @@ input1 = int(input())
 import pandas as pd
 
 input1 = int(input())
+import pandas as pd
+
+input1 = int(input())
+import pandas as pd
+
+input1 = int(input())
+import pandas as pd
+
+input1 = int(input())
+import pandas as pd
+
+input1 = int(input())
+import pandas as pd
+
+input1 = int(input())
