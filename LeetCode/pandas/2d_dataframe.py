e12ba71c3f9a212a9bcc178318001667b1879737
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2024-09-23 22:45:04 +0530

    Updated

--- LeetCode/pandas/2d_dataframe.py
+++ LeetCode/pandas/2d_dataframe.py
@@ -1,3 +1,25 @@
-import pandas as pd
+class A:
+    def add(a,b):
+        print(a+b)
+class B(A):
+    def add(self,a,b):
+        super().add(a,b)
 
-input1 = int(input())
+    def add(self,a,b,c):
+        print(a+b+c)
+
+ob1=B()
+ob1.add(1,2)
+ob1.add(1,2,3)class A:
+    def add(a,b):
+        print(a+b)
+class B(A):
+    def add(self,a,b):
+        super().add(a,b)
+
+    def add(self,a,b,c):
+        print(a+b+c)
+
+ob1=B()
+ob1.add(1,2)
+ob1.add(1,2,3)
