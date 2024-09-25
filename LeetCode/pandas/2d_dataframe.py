dbcedc2ba0f90eaa37d179ff23bf31872eec0b62
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2024-09-25 22:01:55 +0530

    Updated

--- LeetCode/pandas/2d_dataframe.py
+++ LeetCode/pandas/2d_dataframe.py
@@ -718,4 +718,184 @@ class B(A):
 
 ob1=B()
 ob1.add(1,2)
+ob1.add(1,2,3)class A:
+    def add(a,b):
+        print(a+b)
+class B(A):
+    def add(self,a,b):
+        super().add(a,b)
+
+    def add(self,a,b,c):
+        print(a+b+c)
+
+ob1=B()
+ob1.add(1,2)
+ob1.add(1,2,3)class A:
+    def add(a,b):
+        print(a+b)
+class B(A):
+    def add(self,a,b):
+        super().add(a,b)
+
+    def add(self,a,b,c):
+        print(a+b+c)
+
+ob1=B()
+ob1.add(1,2)
+ob1.add(1,2,3)class A:
+    def add(a,b):
+        print(a+b)
+class B(A):
+    def add(self,a,b):
+        super().add(a,b)
+
+    def add(self,a,b,c):
+        print(a+b+c)
+
+ob1=B()
+ob1.add(1,2)
+ob1.add(1,2,3)class A:
+    def add(a,b):
+        print(a+b)
+class B(A):
+    def add(self,a,b):
+        super().add(a,b)
+
+    def add(self,a,b,c):
+        print(a+b+c)
+
+ob1=B()
+ob1.add(1,2)
+ob1.add(1,2,3)class A:
+    def add(a,b):
+        print(a+b)
+class B(A):
+    def add(self,a,b):
+        super().add(a,b)
+
+    def add(self,a,b,c):
+        print(a+b+c)
+
+ob1=B()
+ob1.add(1,2)
+ob1.add(1,2,3)class A:
+    def add(a,b):
+        print(a+b)
+class B(A):
+    def add(self,a,b):
+        super().add(a,b)
+
+    def add(self,a,b,c):
+        print(a+b+c)
+
+ob1=B()
+ob1.add(1,2)
+ob1.add(1,2,3)class A:
+    def add(a,b):
+        print(a+b)
+class B(A):
+    def add(self,a,b):
+        super().add(a,b)
+
+    def add(self,a,b,c):
+        print(a+b+c)
+
+ob1=B()
+ob1.add(1,2)
+ob1.add(1,2,3)class A:
+    def add(a,b):
+        print(a+b)
+class B(A):
+    def add(self,a,b):
+        super().add(a,b)
+
+    def add(self,a,b,c):
+        print(a+b+c)
+
+ob1=B()
+ob1.add(1,2)
+ob1.add(1,2,3)class A:
+    def add(a,b):
+        print(a+b)
+class B(A):
+    def add(self,a,b):
+        super().add(a,b)
+
+    def add(self,a,b,c):
+        print(a+b+c)
+
+ob1=B()
+ob1.add(1,2)
+ob1.add(1,2,3)class A:
+    def add(a,b):
+        print(a+b)
+class B(A):
+    def add(self,a,b):
+        super().add(a,b)
+
+    def add(self,a,b,c):
+        print(a+b+c)
+
+ob1=B()
+ob1.add(1,2)
+ob1.add(1,2,3)class A:
+    def add(a,b):
+        print(a+b)
+class B(A):
+    def add(self,a,b):
+        super().add(a,b)
+
+    def add(self,a,b,c):
+        print(a+b+c)
+
+ob1=B()
+ob1.add(1,2)
+ob1.add(1,2,3)class A:
+    def add(a,b):
+        print(a+b)
+class B(A):
+    def add(self,a,b):
+        super().add(a,b)
+
+    def add(self,a,b,c):
+        print(a+b+c)
+
+ob1=B()
+ob1.add(1,2)
+ob1.add(1,2,3)class A:
+    def add(a,b):
+        print(a+b)
+class B(A):
+    def add(self,a,b):
+        super().add(a,b)
+
+    def add(self,a,b,c):
+        print(a+b+c)
+
+ob1=B()
+ob1.add(1,2)
+ob1.add(1,2,3)class A:
+    def add(a,b):
+        print(a+b)
+class B(A):
+    def add(self,a,b):
+        super().add(a,b)
+
+    def add(self,a,b,c):
+        print(a+b+c)
+
+ob1=B()
+ob1.add(1,2)
+ob1.add(1,2,3)class A:
+    def add(a,b):
+        print(a+b)
+class B(A):
+    def add(self,a,b):
+        super().add(a,b)
+
+    def add(self,a,b,c):
+        print(a+b+c)
+
+ob1=B()
+ob1.add(1,2)
 ob1.add(1,2,3)
