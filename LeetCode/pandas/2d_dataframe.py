789ce60131aeb2e2c180142073df1dc071504535
Sakshi Diggikar <sakshidiggikar681@gmail.com>
2024-10-01 23:02:29 +0530

    Updated

--- LeetCode/pandas/2d_dataframe.py
+++ LeetCode/pandas/2d_dataframe.py
@@ -36,3 +36,22 @@ obj.add(1, 2)
 obj.sub(1, 2)
 obj.mul(1, 2)
 obj.div(1, 2)
+class calculator:
+    def add(a, b):
+        print(a + b)
+
+    def sub(a, b):
+        print(a - b)
+
+    def mul(a, b):
+        print(a * b)
+
+    def div(a, b):
+        print(a / b)
+
+
+obj = calculator()
+obj.add(1, 2)
+obj.sub(1, 2)
+obj.mul(1, 2)
+obj.div(1, 2)
